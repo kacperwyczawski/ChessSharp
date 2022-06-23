ff141f73c37760a1b0cc78399e44ef1a32ffd2c1
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-06-23 15:13:26 +0200

    Add color property to IChessPiece

--- ConsoleChess/ChessPieces/IChessPiece.cs
+++ ConsoleChess/ChessPieces/IChessPiece.cs
@@ -1,10 +1,17 @@
 ﻿namespace ConsoleChess.ChessPieces;
 
+using System.Drawing;
+
 /// <summary>
 /// Represents single chess piece.
 /// </summary>
 public interface IChessPiece
 {
+    /// <summary>
+    /// Gets color of chess piece.
+    /// </summary>
+    public Color Color { get; }
+
     /// <summary>
     /// Converts chess piece to it's <see cref="char"/> representation.
     /// </summary>
