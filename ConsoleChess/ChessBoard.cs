2de72779ae201accefcf2ef6a32a236209ac2ef9
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-06-23 15:06:32 +0200

    Add ChessBoard class

---
+++ ConsoleChess/ChessBoard.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleChess;
+
+using ChessPieces;
+
+/// <summary>
+/// Represents chess board.
+/// </summary>
+public partial class ChessBoard
+{
+    private IChessPiece?[,] chessPiecesArray = StartingChessPiecesArray;
+}
---
+++ ConsoleChess/ChessBoardArraySet.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleChess;
+
+using ChessPieces;
+
+/// <content>
+/// This part of the <see cref="ChessBoard"/> contains a set of useful <see cref="IChessPiece"/> arrays.
+/// </content>
+public partial class ChessBoard
+{
+    private static readonly IChessPiece?[,] StartingChessPiecesArray = new IChessPiece?[8, 8]
+    {
+        { new Rook(), new Knight(), new Bishop(), new Queen(), new King(), new Bishop(), new Knight(), new Rook() },
+        { new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn() },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn(), new Pawn() },
+        { new Rook(), new Knight(), new Bishop(), new Queen(), new King(), new Bishop(), new Knight(), new Rook() },
+    };
+
+    private static readonly IChessPiece?[,] EmptyChessPiecesArray = new IChessPiece?[8, 8]
+    {
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+        { null, null, null, null, null, null, null, null, },
+    };
+}
