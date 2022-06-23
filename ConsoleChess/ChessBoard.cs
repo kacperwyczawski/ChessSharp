fa7617c154520d0b7ae9da80d6ded1f3a1a632b6
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-06-23 18:30:40 +0200

    Add ctor for ChessBoard

--- ConsoleChess/ChessBoard.cs
+++ ConsoleChess/ChessBoard.cs
@@ -7,5 +7,16 @@ using ChessPieces;
 /// </summary>
 public partial class ChessBoard
 {
-    private ChessPiece?[,] chessPiecesArray = StartingChessPiecesArray;
+    private ChessPiece?[,] chessPiecesArray;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChessBoard"/> class.
+    /// </summary>
+    /// <param name="initializeEmpty">
+    /// Describe if <see cref="ChessBoard"/> would be initialized empty.
+    /// </param>
+    public ChessBoard(bool initializeEmpty = false)
+    {
+        this.chessPiecesArray = initializeEmpty ? EmptyChessPiecesArray : StartingChessPiecesArray;
+    }
 }
