3654da5f5f16e21bf2a48bd0dca6ffc9d3bce445
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-08-20 09:59:26 +0200

    Changed the way how move is constructed from string
    For more info see removes TODOs

--- ConsoleChess/Move.cs
+++ ConsoleChess/Move.cs
@@ -58,33 +58,41 @@ public readonly struct Move
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Move"/> struct.
+    ///     Initializes a new instance of the <see cref="Move"/> struct.
     /// </summary>
     /// <param name="moveString">
-    /// string representation of move, for example:
-    /// "1,2 > 1,4 x 1,3" - move piece from cell 1,2 to 1,4 and capture piece on 1,3
-    /// capture piece can be omitted if it is on destination cell, like this:
-    /// "1,2 > 1,4" - move piece from cell 1,2 to 1,4 and capture piece on 1,4
-    /// note: spaces are optional
+    ///     string representation of move. Spaces are optional
+    ///     <example>
+    ///         Remember that almost all moves should have capture cell, except for example regular pawn move<br/>
+    ///         <c>"1,2 > 1,4 x 1,3"</c> - move piece from cell 1,2 to 1,4 and capture piece on 1,3<br/>
+    ///         capture piece can be omitted if it is on destination cell, like this:<br/>
+    ///         <c>"1,2 > 1,4"</c> - move piece from cell 1,2 to 1,4 and capture piece on 1,4<br/>
+    ///         if move doesn't have capture cell, for example regular pawn move, then do it like this:<br/>
+    ///         <c>"1,2 > 1,4 n"</c> - move piece from cell 1,2 to 1,4 without capture cell<br/>
+    ///     </example>
     /// </param>
     public Move(string moveString)
     {
-        // TODO: adjust this to match 5990accba2883566ea9cd162e9b64d03bb27a0ce commit
-        // TODO: move string should still have option to omit capture cell if it is on destination cell
-        // TODO: but should also accept null as capture cell when there is no capture cell
         if (Board == null)
             throw new ArgumentException($"You have to set {nameof(Board)} " +
                                         "property before creating moves with this constructor");
 
         // regex for move string
-        // can be: "n,n > n,n x n,n" or "n,n > n,n"
-        // where n is number between 0 and 7
-        // spaces are optional
-        var regex = new Regex(@"^(?<source>[0-7],[0-7])" // n,n (source cell)
+        // can be:
+        // [1] "a,a > a,a x a,a"
+        // [2] "a,a > a,a"
+        // [3] "a,a > a,a n"
+        // where a is number between 0 and 7
+        
+        // also this regex match [4] "a,a > a,a n a,a" but last cell is anyway ignored.
+        // Why? I don't know how to write proper regex.
+        var regex = new Regex(@"^(?<source>[0-7],[0-7])" // a,a (source cell)
                               + @"\s?>\s?" // > (optionally surrounded by spaces)
-                              + @"(?<destination>[0-7],[0-7])" // n,n (destination cell)
-                              + @"\s?x\s?" // x (optionally surrounded by spaces)
-                              + @"(?<capture>[0-7],[0-7])?$"); // n,n (capture cell)(optional)
+                              + @"(?<destination>[0-7],[0-7])" 
+                              + @"\s?" // optional space
+                              + @"(?<have-capture-cell>[xn])?" // x or n (determine if there is capture cell)(optional)
+                              + @"\s?" // optional space
+                              + @"(?<capture>[0-7],[0-7])?$"); // a,a (capture cell)(optional)
 
         var match = regex.Match(moveString);
 
@@ -92,17 +100,33 @@ public readonly struct Move
         if (!match.Success)
             throw new ArgumentException("Invalid move string", nameof(moveString));
 
+        // ✨ and here is over complicated way to get values from match ✨
         var sourceCell = Board[
             int.Parse(match.Groups["source"].Value[0].ToString()),
             int.Parse(match.Groups["source"].Value[2].ToString())];
+        
         var destinationCell = Board[
             int.Parse(match.Groups["destination"].Value[0].ToString()),
             int.Parse(match.Groups["destination"].Value[2].ToString())];
-        var captureCell = match.Groups["capture"].Success
-            ? Board[
-                int.Parse(match.Groups["capture"].Value[0].ToString()),
-                int.Parse(match.Groups["capture"].Value[2].ToString())]
-            : destinationCell;
+
+        Cell? captureCell;
+        if (match.Groups["have-capture-cell"].Success) // option [1] or [3] or [4]
+        {
+            if (match.Groups["have-capture-cell"].Value == "x") // option [1]
+            {
+                captureCell = Board[
+                    int.Parse(match.Groups["capture"].Value[0].ToString()),
+                    int.Parse(match.Groups["capture"].Value[2].ToString())];
+            }
+            else // option [3] or [4]
+            {
+                captureCell = null;
+            }
+        }
+        else // option [2]
+        {
+            captureCell = destinationCell;
+        }
 
         DestinationCell = destinationCell;
         _sourceCell = sourceCell;
@@ -117,7 +141,7 @@ public readonly struct Move
         // Remove piece from source
         _sourceCell.Piece = null;
 
-        // If there is a capture, remove the captured piece
-        _captureCell.RemovePiece();
+        // If there is a capture cell, remove piece from capture cell
+        _captureCell?.RemovePiece();
     }
 }
