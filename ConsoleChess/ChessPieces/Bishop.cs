bcd700191ed139172575cfd64c82f3f74d2c7250
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-08-22 12:19:51 +0200

    Refactored Bishop.GetValidMoves

--- ConsoleChess/ChessPieces/Bishop.cs
+++ ConsoleChess/ChessPieces/Bishop.cs
@@ -17,42 +17,52 @@ public sealed class Bishop : ChessPiece
     /// <inheritdoc/>
     public override char ToChar() => 'B';
 
-    public override IEnumerable<Cell> GetValidMoves()
+    public override IEnumerable<Move> GetValidMoves()
     {
         // North-West direction
+        // x is decrementing, y is decrementing
         for (int x = Position.X - 1, y = Position.Y - 1; x >= 0 && y >= 0; x--, y--)
         {
+            // always return new move
+            yield return new Move(ParentBoard[x, y],
+                ParentBoard[Position.X, Position.Y]);
+            
+            // if piece hit occupied cell, break
             if (ParentBoard[x, y].IsOccupied)
                 break;
-
-            yield return ParentBoard[x, y];
         }
         
         // North-East direction
+        // x is incrementing, y is decrementing
         for (int x = Position.X + 1, y = Position.Y - 1; x <= 7 && y >= 0; x++, y--)
         {
+            yield return new Move(ParentBoard[x, y],
+                ParentBoard[Position.X, Position.Y]);
+            
             if (ParentBoard[x, y].IsOccupied)
                 break;
-
-            yield return ParentBoard[x, y];
         }
         
         // South-West direction
+        // x is decrementing, y is incrementing
         for (int x = Position.X - 1, y = Position.Y + 1; x >= 0 && y <= 7; x--, y++)
         {
+            yield return new Move(ParentBoard[x, y],
+                ParentBoard[Position.X, Position.Y]);
+            
             if (ParentBoard[x, y].IsOccupied)
                 break;
-
-            yield return ParentBoard[x, y];
         }
         
         // South-East direction
+        // x is incrementing, y is incrementing
         for (int x = Position.X + 1, y = Position.Y + 1; x <= 7 && y <= 7; x++, y++)
         {
+            yield return new Move(ParentBoard[x, y],
+                ParentBoard[Position.X, Position.Y]);
+            
             if (ParentBoard[x, y].IsOccupied)
                 break;
-
-            yield return ParentBoard[x, y];
         }
     }
 }
