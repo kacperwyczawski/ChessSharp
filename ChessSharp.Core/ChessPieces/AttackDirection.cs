5356012eff7b39bb9cd664ede168e70e1ed518f5
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-08-26 17:11:20 +0200

    Added comment in AttackDirection.cs

--- ChessSharp.Core/ChessPieces/AttackDirection.cs
+++ ChessSharp.Core/ChessPieces/AttackDirection.cs
@@ -1,5 +1,7 @@
 ﻿namespace ChessSharp.Core.ChessPieces;
 
+// this may be also struct in the future, with Func<int, int, bool> CanMoveForward with delegate to check if it can
+// and some other fields and also North, South, East and West fields to use it like an enum
 public enum AttackDirection
 {
     North, // white pieces in regular chess
