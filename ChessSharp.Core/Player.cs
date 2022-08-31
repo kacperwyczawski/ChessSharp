b039f4bdfa9ac0c1fb9d603d8dbae1e42086652e
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-08-31 19:52:35 +0200

    Added Player class

---
+++ ChessSharp.Core/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using ChessSharp.Core.BoardRepresentation;
+
+namespace ChessSharp.Core;
+
+public class Player
+{
+    public AttackDirection AttackDirection { get; }
+    public Color Color { get; }
+    public string Name { get; }
+
+    public Player(string name, Color color, AttackDirection attackDirection)
+    {
+        Name = name;
+        Color = color;
+        AttackDirection = attackDirection;
+    }
+}
