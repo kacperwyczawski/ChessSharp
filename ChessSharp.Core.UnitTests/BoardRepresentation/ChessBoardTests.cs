bd127d78c5245a8b45a4535c869f01f10296e881
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-09-01 13:03:27 +0200

    Removed obsolete tests from ChessBoardTests

--- ChessSharp.Core.UnitTests/BoardRepresentation/ChessBoardTests.cs
+++ ChessSharp.Core.UnitTests/BoardRepresentation/ChessBoardTests.cs
@@ -22,45 +22,4 @@ public class ChessBoardTests
         };
         Assert.That(chessBoard.ToCharArray(), Is.EqualTo(expectedArray));
     }
-
-    [Test]
-    public void UseIndexer()
-    {
-        // Arrange
-        ChessBoard chessBoard = new();
-        Knight knight = new(Color.White, chessBoard[1, 1], chessBoard);
-
-        // Act
-        chessBoard[3, 3].Piece = knight;
-        chessBoard[0, 0].Piece = null;
-
-        // Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(chessBoard[3, 3].Piece, Is.EqualTo(knight));
-            Assert.That(chessBoard[0, 0].Piece, Is.EqualTo(null));
-            Assert.That(chessBoard[1, 1].Piece, Is.EqualTo(new Pawn(Color.Black, chessBoard[1, 1], chessBoard, AttackDirection.South)));
-        });
-    }
-
-    [Test]
-    public void MovePiece()
-    {
-        // Arrange
-        ChessBoard board = new();
-        
-        // Act
-        board.MovePiece(board[0, 6].Piece!, board[0, 4]); // Client is responsible to give non-null piece to move
-        board.MovePiece(board[6, 0].Piece!, board[5, 2]);
-        
-        // Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(board[0, 6].Piece, Is.EqualTo(null));
-            Assert.That(board[0, 4].Piece, Is.EqualTo(new Pawn(Color.White, board[0, 4], board, AttackDirection.North)));
-            
-            Assert.That(board[6, 0].Piece, Is.EqualTo(null));
-            Assert.That(board[5, 2].Piece, Is.EqualTo(new Knight(Color.Black, board[5, 2], board)));
-        });
-    }
 }
