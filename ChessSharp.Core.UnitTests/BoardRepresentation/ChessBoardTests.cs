d42e687f619607090d083eef10b8d572d544890f
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-09-10 17:17:47 +0200

    Remove old ChessBoardTests.CreateInstanceWithStartingBoard and add a new UseIndexer test

--- ChessSharp.Core.UnitTests/BoardRepresentation/ChessBoardTests.cs
+++ ChessSharp.Core.UnitTests/BoardRepresentation/ChessBoardTests.cs
@@ -1,25 +1,27 @@
-﻿using ChessSharp.Core.BoardRepresentation;
+﻿using System.Drawing;
+using ChessSharp.Core.BoardRepresentation;
+using ChessSharp.Core.BoardRepresentation.ChessPieces;
 
 namespace ChessSharp.Core.UnitTests.BoardRepresentation;
 
 public class ChessBoardTests
 {
     [Test]
-    public void CreateInstanceWithStartingBoard()
+    public void UseIndexer()
     {
-        ChessBoard chessBoard = new();
-
-        var expectedArray = new char?[,]
+        var p1 = new Player("P1", Color.Blue, AttackDirection.North);
+        var p2 = new Player("P2", Color.Red, AttackDirection.South);
+        
+        var board = new ChessBoard(p1, p2);
+        
+        var piece = board[0, 0].Piece;
+        var pawn = new Pawn(board[3, 3], board, p1);
+        
+        Assert.Multiple(() =>
         {
-            { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' },
-            { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-            { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
-            { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
-        };
-        Assert.That(chessBoard.ToCharArray(), Is.EqualTo(expectedArray));
+            Assert.That(board[0, 0].Piece, Is.EqualTo(piece));
+            Assert.That(board[3, 3].Piece, Is.EqualTo(pawn));
+            Assert.That(board[3, 4].Piece, Is.Null);
+        });
     }
 }
