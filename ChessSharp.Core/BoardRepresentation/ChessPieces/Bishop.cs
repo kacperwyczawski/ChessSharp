3c245bc027fc923e296d469bdb9eb9e3fd26d8bd
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-09-29 09:39:05 +0200

    Fixed Bishop.GetValidMoves

--- ChessSharp.Core/BoardRepresentation/ChessPieces/Bishop.cs
+++ ChessSharp.Core/BoardRepresentation/ChessPieces/Bishop.cs
@@ -16,16 +16,19 @@ public sealed class Bishop : ChessPiece
     public override char ToChar() => 'B';
 
     public override IEnumerable<Move> GetValidMoves()
-    {
+    { 
         // North-West direction
         // x is decrementing, y is decrementing
         for (int x = Position.X - 1, y = Position.Y - 1; x >= 0 && y >= 0; x--, y--)
         {
-            // always return new move
+            // break if the cell is occupied by a piece of the same player
+            if (ReferenceEquals(ParentBoard[x, y].Piece?.Player, Player))
+                break;
+            
             yield return new Move(ParentBoard[x, y],
                 ParentBoard[Position.X, Position.Y]);
             
-            // if piece hit occupied cell, break
+            // break if the cell is occupied by enemy piece
             if (ParentBoard[x, y].IsOccupied)
                 break;
         }
@@ -34,9 +37,14 @@ public sealed class Bishop : ChessPiece
         // x is incrementing, y is decrementing
         for (int x = Position.X + 1, y = Position.Y - 1; x <= 7 && y >= 0; x++, y--)
         {
+            // break if the cell is occupied by a piece of the same player
+            if (ReferenceEquals(ParentBoard[x, y].Piece?.Player, Player))
+                break;
+            
             yield return new Move(ParentBoard[x, y],
                 ParentBoard[Position.X, Position.Y]);
             
+            // break if the cell is occupied by enemy piece
             if (ParentBoard[x, y].IsOccupied)
                 break;
         }
@@ -45,9 +53,14 @@ public sealed class Bishop : ChessPiece
         // x is decrementing, y is incrementing
         for (int x = Position.X - 1, y = Position.Y + 1; x >= 0 && y <= 7; x--, y++)
         {
+            // break if the cell is occupied by a piece of the same player
+            if (ReferenceEquals(ParentBoard[x, y].Piece?.Player, Player))
+                break;
+            
             yield return new Move(ParentBoard[x, y],
                 ParentBoard[Position.X, Position.Y]);
             
+            // break if the cell is occupied by enemy piece
             if (ParentBoard[x, y].IsOccupied)
                 break;
         }
@@ -56,9 +69,14 @@ public sealed class Bishop : ChessPiece
         // x is incrementing, y is incrementing
         for (int x = Position.X + 1, y = Position.Y + 1; x <= 7 && y <= 7; x++, y++)
         {
+            // break if the cell is occupied by a piece of the same player
+            if (ReferenceEquals(ParentBoard[x, y].Piece?.Player, Player))
+                break;
+            
             yield return new Move(ParentBoard[x, y],
                 ParentBoard[Position.X, Position.Y]);
             
+            // break if the cell is occupied by enemy piece
             if (ParentBoard[x, y].IsOccupied)
                 break;
         }
